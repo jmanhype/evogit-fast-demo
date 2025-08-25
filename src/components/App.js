71965eaf9a29cd7981137156b855744f03e860ae
EvoGit <evogit@example.com>
2025-08-24 22:27:19 -0500

    Add timestamps to messages

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,18 @@
 import React from 'react';
-function App() { return <div>Working...</div>; }
+
+class App extends React.Component {
+  state = {
+    timestamp: new Date().toLocaleString()
+  };
+
+  render() {
+    return (
+      <div>
+        Working...
+        <p>Timestamp: {this.state.timestamp}</p>
+      </div>
+    );
+  }
+}
+
 export default App;
